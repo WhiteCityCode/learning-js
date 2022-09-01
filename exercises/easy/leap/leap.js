555213c14afd2c6130b973e224073866f7de9e07
George Cosma <george@whitecitycode.com>
2022-09-01 16:57:10 +0300

    some tests for leap year problem

--- exercises/easy/leap/leap.js
+++ exercises/easy/leap/leap.js
@@ -1,7 +1,8 @@
 /**
  * Return a list of the next `n` leap years.
- * For `n` being 2, the function should return
- * [2024, 2028]
+ *
+ * Example:
+ * n = 2 -> [2024, 2028]
  */
 
 export const leap = (n) => {
