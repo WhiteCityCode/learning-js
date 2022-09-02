e9e5399ea892483c7716dc72aa9de3e7d9093583
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2022-09-02 12:23:21 +0300

    feat: add simplified tic tac toe solution check problem

---
+++ exercises/easy/tictactoe/tictactoe.js
@@ -0,0 +1,29 @@
+/**
+ * Given a 2D matrix of 3x3 elements, filled with either 0 or 1,
+ * check if either of the diagonals is filled with 1s. Return true
+ * if yes, false otherwise.
+ * 
+ * Example:
+ * 
+ * [
+ *  [0, 0, 1],
+ *  [0, 1, 0],
+ *  [1, 0, 0],
+ * ]  => true
+ * 
+ * [
+ *  [1, 0, 1],
+ *  [0, 1, 0],
+ *  [0, 0, 1],
+ * ]  => true
+ * 
+ * [
+ *  [0, 0, 1],
+ *  [0, 1, 0],
+ *  [0, 0, 1],
+ * ]  => false
+ */
+
+export const tictactoe = (grid) => {
+    return false;
+}
