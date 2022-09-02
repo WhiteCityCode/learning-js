d20f0e66e93aa3c6a565bc11b34b99f17d7fb3fd
George Cosma <george@whitecitycode.com>
2022-09-02 10:30:00 +0300

    more problems

---
+++ exercises/easy/evens/evens.js
@@ -0,0 +1,10 @@
+// Given an array of numbers a, return an array b containing only those elements
+// of a that are Even
+
+// Example:
+
+// [1,2,3,4,5,6] -> [2,4,6]
+
+export default function evens(a) {
+  return a;
+}
