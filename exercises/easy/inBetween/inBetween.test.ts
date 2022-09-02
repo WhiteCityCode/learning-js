827fd7205712a428ba79a3f7b7a89368f5eb994d
George Cosma <george@whitecitycode.com>
2022-09-02 15:12:58 +0300

    some more probs

---
+++ exercises/easy/inBetween/inBetween.test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import between from "./inBetween.js";
+
+Deno.test("guess", () => {
+  assertEquals(between([1, 2, 3], 0, 1), [1]);
+  assertEquals(between([1, 2, 3], 0, 2), [1, 2]);
+  assertEquals(between([1, 2, 3], 0, 0), [], "beware the 0");
+});
