1b70a4d823154ef04a3d6cc757d3ce8d60988494
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2022-09-02 17:18:19 +0300

    fix: assert almost equal for floats

--- exercises/easy/americanDistance/americanDistance.test.ts
+++ exercises/easy/americanDistance/americanDistance.test.ts
@@ -1,8 +1,8 @@
 import { americanDistance } from "./americanDistance.js";
-import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+import { assertAlmostEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
 
 Deno.test("americanDistance", () => {
-  assertEquals(americanDistance(1.6), 2.5749504);
-  assertEquals(americanDistance(60), 96.56064);
-  assertEquals(americanDistance(0), 0);
+  assertAlmostEquals(americanDistance(1.6), 2.5749504);
+  assertAlmostEquals(americanDistance(60), 96.56064);
+  assertAlmostEquals(americanDistance(0), 0);
 });
--- exercises/easy/americanHeat/americanHeat.test.ts
+++ exercises/easy/americanHeat/americanHeat.test.ts
@@ -1,9 +1,9 @@
 import { americanHeat } from "./americanHeat.js";
-import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+import { assertAlmostEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
 
 Deno.test("americanHeat", () => {
-  assertEquals(americanHeat(32), 0);
-  assertEquals(americanHeat(100), 37.7777778);
-  assertEquals(americanHeat(-32), -35.5555556);
-  assertEquals(americanHeat(0), -17.7777778);
+  assertAlmostEquals(americanHeat(32), 0);
+  assertAlmostEquals(americanHeat(100), 37.7777778);
+  assertAlmostEquals(americanHeat(-32), -35.5555556);
+  assertAlmostEquals(americanHeat(0), -17.7777778);
 });
