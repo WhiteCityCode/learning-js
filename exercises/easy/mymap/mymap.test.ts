ac66610643de62a9342c9b12a3725679c915375d
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2022-09-02 15:11:42 +0300

    fix: add test for mymap

---
+++ exercises/easy/mymap/mymap.test.ts
@@ -0,0 +1,35 @@
+import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+import { mymap } from "./mymap.js";
+
+Deno.test("mymap", () => {
+  assertEquals(
+    mymap([1, 2, 3, 4], (n: number) => `${n}`),
+    ["1", "2", "3", "4"]
+  );
+  assertEquals(
+    mymap([1, 2, 3, 4], (n: number) => n * 2),
+    [2, 4, 6, 8]
+  );
+  assertEquals(
+    mymap([], (n: number) => n),
+    []
+  );
+  assertEquals(
+    mymap([1, 2, 3, 4], (n: number) => n),
+    [1, 2, 3, 4]
+  );
+  assertEquals(
+    mymap([1, 2, 3, 4], (n: number) => [n]),
+    [[1], [2], [3], [4]]
+  );
+  assertEquals(
+    mymap([{ foo: 1 }, { bar: 2 }, { baz: 3 }], (n: Record<string, number>) =>
+      Object.entries(n)
+    ),
+    [
+      ["foo", 1],
+      ["bar", 2],
+      ["baz", 3],
+    ]
+  );
+});
