bf360443a87640ec4b1c5a513389350eff56213f
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2022-09-02 15:07:51 +0300

    feat: add square problem

---
+++ exercises/easy/square/square.js
@@ -0,0 +1,10 @@
+/**
+ * Given a number n, find the next smallest perfect square
+ * (a perfect square is a number that can be expressed as a product of two
+ * that is, it is the square of an integer). For example, if n = 13, the next
+ * smallest perfect square would be 16.
+ */
+
+export const square = (n) => {
+    return n;
+};
---
+++ exercises/easy/square/square.test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
+import { square } from "./square.js";
+
+Deno.test("square", () => {
+  assertEquals(square(13), 16);
+  assertEquals(square(1), 4);
+  assertEquals(square(0), 1);
+});
