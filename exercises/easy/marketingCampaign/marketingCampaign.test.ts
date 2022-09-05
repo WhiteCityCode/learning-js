d3fbf889a7ef4e6cf07e0f3a94b6dfa6573bbaa6
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2022-09-05 13:48:25 +0300

    feat: faulty marketing campaign problem

---
+++ exercises/easy/marketingCampaign/marketingCampaign.test.ts
@@ -0,0 +1,29 @@
+import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import { applyDiscount } from "./marketingCampaign.js";
+
+Deno.test("guess", () => {
+  assertEquals(applyDiscount([{ name: "Foo", price: 5 }]), [
+    { name: "Foo", price: 5, discount: 0 },
+  ]);
+
+  assertEquals(applyDiscount([{ name: "Foo", price: 15 }]), [
+    { name: "Foo", price: 15, discount: 15 },
+  ]);
+
+  assertEquals(applyDiscount([{ name: "Foo", price: 150 }]), [
+    { name: "Foo", price: 150, discount: 10 },
+  ]);
+
+  assertEquals(
+    applyDiscount([
+      { name: "Foo", price: 150 },
+      { name: "Bar", price: 50 },
+      { name: "Baz", price: 5 },
+    ]),
+    [
+      { name: "Foo", price: 150, discount: 10 },
+      { name: "Bar", price: 50, discount: 15 },
+      { name: "Baz", price: 5, discount: 0 },
+    ]
+  );
+});
