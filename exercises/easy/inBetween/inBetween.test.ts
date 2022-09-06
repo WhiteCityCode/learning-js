f6134483cec699e12037984b582ddc695f3dcab5
George Cosma <george@whitecitycode.com>
2022-09-06 12:38:12 +0300

    fix: change inBetween tests to test for indexes that contain other values than themselves

--- exercises/easy/inBetween/inBetween.test.ts
+++ exercises/easy/inBetween/inBetween.test.ts
@@ -3,7 +3,8 @@ import between from "./inBetween.js";
 
 Deno.test("guess", () => {
   assertEquals(between([1, 2, 3], 0, 1), [1]);
-  assertEquals(between([1, 2, 3], 0, 2), [1, 2]);
+  assertEquals(between([5, 2, 7], 0, 2), [5, 2]);
+  assertEquals(between([5, 2, 7, 2, 6, 9, 0], 3, 6), [2, 6, 9]);
   assertEquals(between([1, 2, 3], 0, 0), [], "beware the 0");
   assertEquals(between([1, 2, 3], 2, 2), []);
 });
