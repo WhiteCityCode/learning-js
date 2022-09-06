fe60a9044f65bd8442e9830254b5bca025e54b78
George Cosma <george@whitecitycode.com>
2022-09-06 13:06:11 +0300

    feat: more tests for inbetween

--- exercises/easy/inBetween/inBetween.test.ts
+++ exercises/easy/inBetween/inBetween.test.ts
@@ -5,6 +5,7 @@ Deno.test("guess", () => {
   assertEquals(between([1, 2, 3], 0, 1), [1]);
   assertEquals(between([5, 2, 7], 0, 2), [5, 2]);
   assertEquals(between([5, 2, 7, 2, 6, 9, 0], 3, 6), [2, 6, 9]);
+  assertEquals(between([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 2, 5), [0, 0, 0]);
   assertEquals(between([1, 2, 3], 0, 0), [], "beware the 0");
   assertEquals(between([1, 2, 3], 2, 2), []);
 });
