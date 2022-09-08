0548d06a26792457532bbf3af265dc053a01db0f
George Cosma <george@whitecitycode.com>
2022-09-08 16:33:33 +0300

    fix:reverted claudiu's mistake

--- exercises/70-geroge/geroge.test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
-import { geroge } from "./geroge.ts";
-
-Deno.test("geroge", () => {
-  assertEquals(geroge(), []);
-});
