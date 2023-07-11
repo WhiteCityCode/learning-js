43dcd3489dfacd4c965a103b29f4bebb31652d5a
Tronciu Dumitru <72997097+Parsyfall@users.noreply.github.com>
2023-07-11 22:16:07 +0300

    Fix: add parameters and expected return for test
    * fix: fix typo on test 37
    
    * feat: added new test for problem 28
    
    * Fix: add parameters and expected return for test
    
    - Added parameters for function call and it's expected value in test for
      exercise 93

--- exercises/93-search/search.test.ts
+++ exercises/93-search/search.test.ts
@@ -2,5 +2,6 @@ import { assertEquals } from "https://deno.land/std@0.154.0/testing/asserts.ts";
 import { search } from "./search.ts";
 
 Deno.test("search", () => {
-  assertEquals(search(), []);
+  assertEquals(search([{name: 'John', age: 20}, {name: 'Jane', age: 21}], 'Jane', ['name']),
+			   [{name: 'Jane', age: 21}]);
 });
